29d5b415039da564cb59074f715514ab2343d08f
SergeyDff <130085050+SergeyDff@users.noreply.github.com>
2023-04-09 13:40:48 +0300

    Update game_search_number.py

--- game_search_number.py
+++ game_search_number.py
@@ -6,12 +6,12 @@ def search_number():
     while True :
         player1 = int(random.randint(1,5))
         player2 = int(input('угадай число от 1 до 5 '))
-        otvet = player1 - player2
+        answer = player1 - player2
         if player1 == player2 :
             print('угадал удача на твоей стороне')
             yes_or_no = input('хотите ли вы продолжить ')
         else:
-            print(f'не угадал тебе не хватило {otvet} попробуй еще раз мало ли в следующий раз провезет')
+            print(f'не угадал тебе не хватило {answer} попробуй еще раз мало ли в следующий раз провезет')
             yes_or_no = input('хотите ли вы продолжить ')
         if yes_or_no == 'нет':
             break
