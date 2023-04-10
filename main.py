8c1335a7c9d4cb6785b233e05020b0821673f179
Valerii57586 <120032748+Valerii57586@users.noreply.github.com>
2023-04-10 21:37:28 +0300

    добавил новую,но не до конца доделаную игру

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import game_search_number
+import game_search_number,dice.paradice
 print('наш католог:','угадай число','стань ближе к нужному','дота пикер')
 game = input('выбери игру которую хочешь погеймить')
 if game == 'угадай число':
     game_search_number.search_number()
+if game == 'стань ближе к нужному':
+    dice.paradice.dice()
 
 
 
