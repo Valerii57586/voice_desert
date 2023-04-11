832e9517fe8a80aff21491399fcc03a7f6bcb08c
Valerii57586 <120032748+Valerii57586@users.noreply.github.com>
2023-04-11 21:03:32 +0300

    доделал

--- dice_paradice.py
+++ dice_paradice.py
@@ -9,12 +9,23 @@ def dice():
     something = [number_which_need_to_hide,player1,player2]
     something.sort()
     winner = something[1]
+    s = winner
     if player1 == winner :
         print(' игрок под номером 1 оказался ближе ')
     if player2 == winner :
         print('игрок под номером 2 оказался ближе ')
+    if number_which_need_to_hide == winner :
+        a = player1 - winner
+        b = player2 - s
+        if abs(a) > abs(b) :
+            print('игрок под номером 2 оказался ближе')
+        if abs(a) < abs(b) :
+            print('игрок под номером 1 оказался ближе')
+    print(f'таблица{something}')
 
 
 
 if __name__ == '__main__' :
     dice()
+
+
