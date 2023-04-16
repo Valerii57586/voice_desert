4e04f6f12dbad2243c1381488896496cd09beade
Valerii57586 <120032748+Valerii57586@users.noreply.github.com>
2023-04-16 13:44:46 +0300

    Add files via upload

---
+++ reactionality.py
@@ -0,0 +1,12 @@
+import random
+from datetime import datetime
+import time
+ABC = 'qwertyuopasdfghjklzxcvbnm'
+
+
+def reactionality():
+    ABC = 'qwertyuopasdfghjklzxcvbnm'
+    symbol = random.randint(1,27)
+    while :
+        start_time = datetime.now()
+        player = input()
